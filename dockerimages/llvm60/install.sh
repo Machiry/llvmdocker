664303fcd6413300a62d056c0d0682207679a136
Aravind Machiry <machiry@cs.ucsb.edu>
2019-02-28 14:28:29 -0800

    Initial commit

---
+++ dockerimages/llvm60/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+source /etc/bash_completion
+mkdir /setup
+cd /setup
+git clone https://github.com/Machiry/llvmdocker.git llvmdocker
+cd llvmdocker/dockerimages/setupscripts
+python setup_llvm.py -o /tools/llvmdeps -b release_60
+echo 'export LLVM_ROOT=/tools/llvmdeps/llvm/build' >> ~/.bashrc
+echo 'export PATH=$LLVM_ROOT/bin:/tools/llvmdeps/sparse:$PATH' >> ~/.bashrc
+export LLVM_ROOT=/tools/llvmdeps/llvm/build
+export PATH=$LLVM_ROOT/bin:/tools/llvmdeps/sparse:$PATH
